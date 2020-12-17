5fcc1556524a40b307f1c6bfafbcff951300e76e
Samster91 <cester.samuele@gmail.com>
2020-12-17 18:28:37 +0100

    Updates

---
+++ precache-manifest.172f2536a035a7c85921121621c7d6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd96bff467184e287b87",
+    "url": "/static/js/main.dd96bff4.chunk.js"
+  },
+  {
+    "revision": "1f2556ed80eea502b28f",
+    "url": "/static/js/1.1f2556ed.chunk.js"
+  },
+  {
+    "revision": "dd96bff467184e287b87",
+    "url": "/static/css/main.08cbc081.chunk.css"
+  },
+  {
+    "revision": "1f2556ed80eea502b28f",
+    "url": "/static/css/1.c92b7e04.chunk.css"
+  },
+  {
+    "revision": "7aef184fcd92d75fa6bc0cd5926ba828",
+    "url": "/index.html"
+  }
+];
