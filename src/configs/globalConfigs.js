1dce452b355d0b3242d83847b6e4c94066618d21
dhruv035 <dhruv035@gmail.com>
2022-01-19 18:33:24 +0530

    FIX:Conform to Subgraph

--- src/configs/globalConfigs.js
+++ src/configs/globalConfigs.js
@@ -1733,6 +1733,7 @@ const globalConfigs = {
               'blockNumber',
               'totalSupply',
               'virtualPrice',
+              'contractValue',
             ]
           }
         }
