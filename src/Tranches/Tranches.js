f69eada04c6fa716794fbc3b78f8d25d32b26d54
dhruv035 <dhruv035@gmail.com>
2022-03-02 23:27:18 +0530

    fix margins

--- src/Tranches/Tranches.js
+++ src/Tranches/Tranches.js
@@ -930,14 +930,14 @@ class Tranches extends Component {
                     {
                       title:'APY',
                       props:{
-                        width:[0.18, 0.08],
+                        width:[0.18, 0.1],
                       },
                       fields:[
                         {
-                          name:'trancheApyWithTooltip',
+                          name:'trancheApy',
                           props:{
                             mr:2,
-                            height:['1.4em','2em']
+                            size:'1.4em'
                           }
                         },
                       ]
@@ -984,7 +984,7 @@ class Tranches extends Component {
                     {
                       title:'DEPOSITED',
                       props:{
-                        width:[0.29,0.14],
+                        width:[0.29,0.12],
                       },
                       fields:[
                         {
@@ -1001,7 +1001,7 @@ class Tranches extends Component {
                     {
                       title:'EARNINGS',
                       props:{
-                        width:[0.29,0.13],
+                        width:[0.29,0.16],
                         textAlign:'center'
                       },
                       parentProps:{
