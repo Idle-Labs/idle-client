813812b759baf0440c9abad7d9e7620f9afd12b4
dhruv035 <dhruv035@gmail.com>
2022-03-11 22:07:25 +0530

    Remove 1

--- src/utilities/FunctionsUtil.js
+++ src/utilities/FunctionsUtil.js
@@ -310,7 +310,6 @@ class FunctionsUtil {
       totalAmountLent: this.BNify(0),
       totalEarningsPerc: this.BNify(0),
     };
-    console.log('1')
 
     availableTranches = availableTranches ? availableTranches : this.props.availableTranches;
     account = account ? account : this.props.account;
@@ -447,7 +446,7 @@ class FunctionsUtil {
         totalBalance = totalBalance.plus(trancheInfo.tokenBalance);
       }
     });
-    
+
     if (totalAmountLent.gt(0)) {
       totalEarningsPerc = totalEarnings.div(totalAmountLent).times(100);
     }
